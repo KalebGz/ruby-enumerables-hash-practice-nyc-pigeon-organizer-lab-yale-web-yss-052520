dda8dc4d9ba69c2735f74112bb9d3ada440a3ff4
Kaleb Gezahegn <kaleb.gezahegn@yale.edu>
2020-05-20 11:01:20 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,14 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   
   # Create an array of all the names found
-  
+  res = fata.each_with_object({}){ |key, value|, final_array
+    
+    value.each{|inner_key, names|
+      names.each{ |name|
+        if !final_array[name]
+          final_hash[name] = {}
+        end
+      }
+    }
+  }
 end
